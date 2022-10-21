01de3479bde0a9d16cadfc8c5f1647a30a7b3fcb
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-21 10:19:49 +0700

    add encapsulation

--- angular-master/src/app/user-data/output-data/output-data.component.ts
+++ angular-master/src/app/user-data/output-data/output-data.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-output-data',
   templateUrl: './output-data.component.html',
   styleUrls: ['./output-data.component.css'],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class OutputDataComponent implements OnInit {
   @Input() UserData!: { userName: string; statusName: string };
--- angular-master/src/app/user-data/user-data.component.ts
+++ angular-master/src/app/user-data/user-data.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class UserDataComponent implements OnInit {
   constructor() {}
