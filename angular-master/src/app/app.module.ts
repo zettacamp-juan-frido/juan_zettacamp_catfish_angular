4975940d7c8d734575b27a4a130178dd75afc589
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-24 13:06:25 +0700

    initial component and module

--- angular-master/src/app/app.module.ts
+++ angular-master/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardListModule } from './card-list/card-list.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CardListModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-master/src/app/card-list/card-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardListComponent } from './card-list.component';
+import { CardUserComponent } from './card-user/card-user.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+
+@NgModule({
+  declarations: [CardListComponent, CardUserComponent, CardDetailComponent],
+  imports: [CommonModule],
+})
+export class CardListModule {}
