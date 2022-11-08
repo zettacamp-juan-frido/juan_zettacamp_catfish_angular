4bd8c41e136f06cae2a3b39b617100b345976134
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-11-08 12:10:04 +0700

    fix folder and file

--- angular-master/src/app/app.module.ts
+++ angular-master/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './container/material/material.module';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
